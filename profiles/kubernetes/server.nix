3b4ee5be548f7e3c2c52fea11a071d84298f83f4
Oliver Ni <oliver.ni@gmail.com>
2022-12-07 13:13:00 -0800

    Remove serverAddr parameter

--- profiles/kubernetes/server.nix
+++ profiles/kubernetes/server.nix
@@ -8,7 +8,6 @@
     enable = true;
     role = "server";
     tokenFile = config.age.secrets.k3s-server-token.path;
-    serverAddr = "https://control-plane.poketwo.io:6443";
     extraFlags = toString [
       "--tls-san=control-plane.poketwo.io"
       "--node-taint=CriticalAddonsOnly=true:NoExecute"
