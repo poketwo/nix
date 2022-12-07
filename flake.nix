8f57c12be5709d42cab4aface020c9bf740053e1
Oliver Ni <oliver.ni@gmail.com>
2022-12-07 12:31:12 -0800

    Upgrade to nixpkgs 22.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Configuration for Pokétwo";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/22.05;
+    nixpkgs.url = github:nixos/nixpkgs/22.11;
     flake-utils-plus.url = github:gytis-ivaskevicius/flake-utils-plus/v1.3.1;
     agenix.url = "github:ryantm/agenix/0.13.0";
   };
