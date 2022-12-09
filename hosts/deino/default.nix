5e734dfca9b7983bf506479738028d3dcd2cfb70
Oliver Ni <oliver.ni@gmail.com>
2022-12-08 18:03:46 -0800

    Add k3s agent to deino

--- hosts/deino/default.nix
+++ hosts/deino/default.nix
@@ -1,8 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware.nix ];
+  imports = [
+    ./hardware.nix
+    ../../profiles/kubernetes/agent.nix
+  ];
 
   networking.hostName = "deino";
+  services.kubernetes.node-ip = "100.114.243.119";
   system.stateVersion = "22.05";
 }
