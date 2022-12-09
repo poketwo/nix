84a8514d7361a80fc7c83c32172086e8c0719a07
Oliver Ni <oliver.ni@gmail.com>
2022-12-08 22:49:24 -0800

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p git
+
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root!"
+  exit
+fi
+
+echo "Writing bootstrap configuration..."
+
+cat <<EOF >>bootstrap.nix
+{ pkgs, ... }: {
+  imports = [ ./configuration.nix ];
+  environment.systemPackages = [ pkgs.tailscale ];
+  services.tailscale.enable = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
+EOF
+
+echo "Building bootstrap configuration..."
+
+nixos-rebuild switch -I nixos-config=./bootstrap.nix
+
+echo "Starting tailscale..."
+
+tailscale up --ssh
+
+echo "Downloading new configs..."
+
+rm -rf *
+git clone https://github.com/oliver-ni/nix.git .
+
+echo "Building configs..."
+
+nixos-rebuild switch
