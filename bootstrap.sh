bab5b54733656c2aa3bd7a168b403c1930aa2ffa
Oliver Ni <oliver.ni@gmail.com>
2022-12-09 20:40:54 -0800

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Writing bootstrap configuration..."
 
-cat <<EOF >>bootstrap.nix
+cat <<EOF >bootstrap.nix
 { pkgs, ... }: {
   imports = [ ./configuration.nix ];
   environment.systemPackages = [ pkgs.tailscale ];
@@ -26,6 +26,8 @@ nixos-rebuild switch -I nixos-config=./bootstrap.nix
 echo "Starting tailscale..."
 
 tailscale up --ssh
+
+read -p "Upload configs, then press enter to continue..."
 
 echo "Downloading new configs..."
 
