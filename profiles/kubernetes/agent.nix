7e47bf43095d005d1b50eedfaa5be10b0d0d69c0
Oliver Ni <oliver.ni@gmail.com>
2023-06-30 11:48:17 -0700

    Allow metallb port in firewall

--- profiles/kubernetes/agent.nix
+++ profiles/kubernetes/agent.nix
@@ -34,8 +34,9 @@ in
   networking.firewall = {
     # https://docs.k3s.io/installation/requirements#networking
     # https://docs.cilium.io/en/v1.11/operations/system_requirements/#firewall-rules
+    # https://metallb.universe.tf
 
-    allowedTCPPorts = [ 10250 4240 4244 ];
-    allowedUDPPorts = [ 8472 ];
+    allowedTCPPorts = [ 10250 4240 4244 7946 ];
+    allowedUDPPorts = [ 8472 7946 ];
   };
 }
