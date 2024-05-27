6c9b6dd45f66ec4181003b82e9d9bdc67c83d4ae
Oliver Ni <oliverni@berkeley.edu>
2024-05-27 16:43:54 -0400

    feat: add cilium

---
+++ kubernetes/cilium.nix
@@ -0,0 +1,64 @@
+{ transpire, ... }:
+
+{
+  namespaces.cilium = {
+    helmReleases.cilium = {
+      chart = transpire.fetchFromHelm {
+        repo = "https://helm.cilium.io/";
+        name = "cilium";
+        version = "1.15.5";
+        sha256 = "33J0dGfxtw5qfMBZGl6C2xyt5xE2AT+fGLJNrNKwM6g=";
+      };
+
+      values = {
+        # We use Kubernetes IPAM, but KCM isn't configured to allocate addresses
+        # since we need more control over the CIDRs (disjoint).
+        # So, we apply the network.cilium.io/ipv6-pod-cidr annotation on Node
+        ipam.mode = "kubernetes";
+        annotateK8sNode = true;
+
+        # Enable Cilium's kube-proxy replacement.
+        # https://docs.cilium.io/en/stable/network/kubernetes/kubeproxy-free/
+        kubeProxyReplacement = true;
+        k8sServiceHost = "ds.hfym.co";
+        k8sServicePort = "443";
+
+        # TODO: Look into this
+        # nat46x64Gateway.enabled = true;
+
+        # Cilium L2 Announcements doesn't currently support IPv6/NDP.
+        # When it does, we can consider using this instead of MetalLB.
+        # https://docs.cilium.io/en/stable/network/l2-announcements/
+        # l2announcements.enabled = true;
+
+        # Cilium Kubernetes Ingress Controller
+        # https://docs.cilium.io/en/stable/network/servicemesh/ingress/
+        ingressController = {
+          enable = true;
+          default = true;
+          loadbalancerMode = "shared";
+        };
+
+        # Apparently this is good so I'm turning it on
+        endpointRoutes.enabled = true;
+
+        # Maybe look into Gateway API (Ingress successor) in the future
+        # https://docs.cilium.io/en/stable/network/servicemesh/gateway-api/gateway-api/
+        # gatewayAPI.enabled = true;
+
+        # This cluter is IPv6-only.
+        ipv4.enabled = false;
+        ipv6.enabled = true;
+        enableIPv6Masquerade = false;
+
+        # Metrics
+        prometheus.enabled = true;
+        dashboards.enabled = true;
+
+        # Native mode delegates routing to node's networking stack
+        # We can do this because IPV6, hopefully
+        routingMode = "native";
+      };
+    };
+  };
+}
