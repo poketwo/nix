033513bc5c3f10b864a7100e2151e63892fed1c1
Oliver Ni <oliverni@berkeley.edu>
2024-06-14 18:42:47 -0400

    fix: make cilium work

--- kubernetes/cilium.nix
+++ kubernetes/cilium.nix
@@ -6,25 +6,43 @@
       chart = transpire.fetchFromHelm {
         repo = "https://helm.cilium.io/";
         name = "cilium";
-        version = "1.15.5";
-        sha256 = "33J0dGfxtw5qfMBZGl6C2xyt5xE2AT+fGLJNrNKwM6g=";
+        version = "1.15.6";
+        sha256 = "PRKAtv1dCQUXuWF7wixWMl0XZVU50FKdUX3euwB8s+Y=";
       };
 
       values = {
+        extraArgs = [ "--direct-routing-device=inet0" ];
+
         # We use Kubernetes IPAM, but KCM isn't configured to allocate addresses
-        # since we need more control over the CIDRs (disjoint).
+        # since we need more control over the CIDRs (which are disjoint).
         # So, we apply the network.cilium.io/ipv6-pod-cidr annotation on Node
         ipam.mode = "kubernetes";
         annotateK8sNode = true;
 
-        # Enable Cilium's kube-proxy replacement.
+        # Enable Cilium's kube-proxy replacement in Hybrid DSR/SNAT mode
         # https://docs.cilium.io/en/stable/network/kubernetes/kubeproxy-free/
         kubeProxyReplacement = true;
         k8sServiceHost = "ds.hfym.co";
         k8sServicePort = "443";
+        loadBalancer.mode = "hybrid";
+        endpointRoutes.enabled = true;
 
-        # TODO: Look into this
-        # nat46x64Gateway.enabled = true;
+        # We use globally routable addresses for IPv6, so no NAT is needed.
+        # We're running IPv6-only, but Discord still needs IPv4...
+        # For now, that is accomplished with NAT64 on the host. :)
+        ipv4.enabled = false;
+        ipv6.enabled = true;
+        routingMode = "native";
+        enableIPv6Masquerade = false;
+
+        # Observability things
+        prometheus.enabled = true;
+        dashboards.enabled = true;
+        hubble = {
+          tls.auto.method = "cronJob";
+          relay.enabled = true;
+          ui.enabled = true;
+        };
 
         # Cilium L2 Announcements doesn't currently support IPv6/NDP.
         # When it does, we can consider using this instead of MetalLB.
@@ -33,48 +51,19 @@
 
         # Cilium Kubernetes Ingress Controller
         # https://docs.cilium.io/en/stable/network/servicemesh/ingress/
-        ingressController = {
-          enable = true;
-          default = true;
-          loadbalancerMode = "shared";
-        };
-
-        # Apparently this is good so I'm turning it on
-        endpointRoutes.enabled = true;
-
-        # Maybe look into Gateway API (Ingress successor) in the future
-        # https://docs.cilium.io/en/stable/network/servicemesh/gateway-api/gateway-api/
-        # gatewayAPI.enabled = true;
-
-        # This cluter is IPv6-only.
-        ipv4.enabled = false;
-        ipv6.enabled = true;
-        enableIPv6Masquerade = false;
-
-        # Metrics
-        prometheus.enabled = true;
-        dashboards.enabled = true;
-
-        # Native mode delegates routing to node's networking stack
-        # We can do this because IPV6, hopefully
-        routingMode = "native";
-
-        hubble = {
-          tls.auto.method = "cronJob";
-          relay.enabled = true;
-          ui.enabled = true;
-        };
+        # Currently broken :(
+        # ingressController = {
+        #   enabled = true;
+        #   default = true;
+        # };
+        # envoy.enabled = true;
       };
     };
 
     resources."cilium.io/v2".CiliumClusterwideNetworkPolicy.deny-external.spec = {
       endpointSelector = { };
       ingress = [{ fromEntities = [ "cluster" ]; }];
-    };
-
-    resources.v1.Service.hedgedoc.spec = {
-      ports = [{ port = 80; targetPort = 3000; }];
-      selector.app = "hedgedoc";
+      egress = [{ toEntities = [ "all" ]; }];
     };
   };
 }
