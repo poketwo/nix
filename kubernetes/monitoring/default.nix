ee017d52803a152e45e243d65a076c7d27835d7c
Oliver Ni <oliverni@berkeley.edu>
2024-06-22 15:59:46 -0400

    feat(monitoring): add root hns

---
+++ kubernetes/monitoring/default.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  namespaces.monitoring.resources."hnc.x-k8s.io/v1alpha2".SubnamespaceAnchor = {
+    alloy = { };
+    grafana = { };
+    loki = { };
+    mimir = { };
+    prometheus = { };
+  };
+}
