399152ce7de5c125df9b75847436b66920ee42f7
Oliver Ni <oliverni@berkeley.edu>
2024-06-23 19:14:22 -0400

    feat: add tailscale operator

---
+++ kubernetes/core/tailscale.nix
@@ -0,0 +1,26 @@
+{ transpire, ... }:
+
+{
+  namespaces.tailscale = {
+    helmReleases.tailscale-operator = {
+      chart = transpire.fetchFromHelm {
+        repo = "https://pkgs.tailscale.com/helmcharts";
+        name = "tailscale-operator";
+        version = "1.68.1";
+        sha256 = "3j+DRDFF/iPvgGlyXFw2riniHwEb1diFKeMLb3Kp+HA=";
+      };
+
+      values = {
+        operatorConfig.defaultTags = [ "tag:hfym-ds" ];
+      };
+    };
+
+    resources.v1.Secret.operator-oauth = {
+      type = "Opaque";
+      stringData = {
+        client_id = "";
+        client_secret = "";
+      };
+    };
+  };
+}
