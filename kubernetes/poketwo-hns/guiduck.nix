b010f7829cb3afa6920ad439677b76dd60a6c49d
Oliver Ni <oliverni@berkeley.edu>
2024-06-24 22:17:10 -0400

    fix(guiduck): fix mongodb config

--- kubernetes/poketwo-hns/guiduck.nix
+++ kubernetes/poketwo-hns/guiduck.nix
@@ -2,17 +2,15 @@
 
 let
   makeLoadBalancer = index: {
-    metadata.annotations = {
-      "external-dns.alpha.kubernetes.io/hostname" = "mongodb-${index}.guiduck-mongodb.svc.poketwo.io";
-      "external-dns.alpha.kubernetes.io/cloudflare-proxied" = "false";
-    };
+    metadata.labels."tailscale.com/proxy-class" = "prod";
     spec = {
       selector = {
-        app = "guiduck-mongodb-svc";
-        "statefulset.kubernetes.io/pod-name" = "guiduck-mongodb-${index}";
+        app = "mongodb-svc";
+        "statefulset.kubernetes.io/pod-name" = "mongodb-${index}";
       };
       ports = [{ port = 27017; }];
       type = "LoadBalancer";
+      loadBalancerClass = "tailscale";
     };
   };
 in
@@ -20,36 +18,39 @@ in
   namespaces.guiduck = {
     createNamespace = false;
 
-    resources."cert-manager.io/v1".Certificate.guiduck-mongodb-tls.spec = {
-      commonName = "*.guiduck-mongodb-svc.guiduck.svc.cluster.local";
-      dnsNames = [ "*.guiduck-mongodb-svc.guiduck.svc.cluster.local" ];
-      secretName = "guiduck-mongodb-tls";
+    resources."cert-manager.io/v1".Certificate.mongodb-tls.spec = {
+      commonName = "*.mongodb-svc.guiduck.svc.cluster.local";
+      dnsNames = [ "*.mongodb-svc.guiduck.svc.cluster.local" ];
+      secretName = "mongodb-tls";
       issuerRef = {
         name = "cluster-ca";
         kind = "ClusterIssuer";
       };
     };
 
-    resources."mongodbcommunity.mongodb.com/v1".MongoDBCommunity.guiduck-mongodb.spec = {
+    resources."mongodbcommunity.mongodb.com/v1".MongoDBCommunity.mongodb.spec = {
       type = "ReplicaSet";
       members = 3;
       version = "7.0.11";
       security.tls = {
         enabled = true;
-        certificateKeySecretRef.name = "guiduck-mongodb-tls";
+        certificateKeySecretRef.name = "mongodb-tls";
         caCertificateSecretRef.name = "cluster-ca";
       };
       users = [{
         name = "guiduck";
         scramCredentialsSecretName = "guiduck";
-        passwordSecretRef.name = "guiduck-mongodb-user";
+        passwordSecretRef.name = "mongodb-user";
         roles = [{ name = "root"; db = "admin"; }];
       }];
       additionalMongodConfig."net.ipv6" = true;
       statefulSet.spec.template.spec = {
         volumeClaimTemplates = [{
           metadata.name = "data-volume";
-          spec.resouces.requests.storage = "256Gi";
+          spec = {
+            resouces.requests.storage = "256Gi";
+            storageClassName = "rbd-nvme-retain";
+          };
         }];
         containers = [{
           name = "mongod";
@@ -60,26 +61,26 @@ in
         }];
       };
       replicaSetHorizons = [
-        { external = "mongodb-0.guiduck-mongodb.svc.poketwo.io:27017"; }
-        { external = "mongodb-1.guiduck-mongodb.svc.poketwo.io:27017"; }
-        { external = "mongodb-2.guiduck-mongodb.svc.poketwo.io:27017"; }
+        { external = "guiduck-mongodb-0-external.royal-pinecone.ts.net:27017"; }
+        { external = "guiduck-mongodb-1-external.royal-pinecone.ts.net:27017"; }
+        { external = "guiduck-mongodb-2-external.royal-pinecone.ts.net:27017"; }
       ];
     };
 
-    resources.v1.Secret."guiduck-mongodb-user" = {
+    resources.v1.Secret."mongodb-user" = {
       type = "Opaque";
       stringData.password = "";
     };
 
     resources.v1.Service = builtins.listToAttrs (map
       (index: {
-        name = "guiduck-mongodb-${index}-external";
+        name = "mongodb-${index}-external";
         value = makeLoadBalancer index;
       }) [ "0" "1" "2" ]);
 
-    resources."cilium.io/v2".CiliumNetworkPolicy.allow-external-ingress-to-mongodb.spec = {
-      endpointSelector.matchLabels.app = "guiduck-mongodb-svc";
-      ingress = [{ fromEntities = [ "all" ]; }];
-    };
+    # resources."cilium.io/v2".CiliumNetworkPolicy.allow-tailnet-ingress-to-mongodb.spec = {
+    #   endpointSelector.matchLabels.app = "mongodb-svc";
+    #   ingress = [{ fromCIDR = [ "fd7a:115c:a1e0::/96" ]; }];
+    # };
   };
 }
