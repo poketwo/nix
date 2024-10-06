6208dfe25a07ee04ed9e7d5de319fe5d9a089668
Oliver Ni <oliverni@berkeley.edu>
2024-10-06 11:46:26 -0700

    feat: add role bindings for witherr in other namespaces

--- kubernetes/users/witherr.nix
+++ kubernetes/users/witherr.nix
@@ -1,32 +1,27 @@
 { ... }:
 
+let
+  adminRoleBinding = {
+    subjects = [{
+      kind = "ServiceAccount";
+      name = "witherr";
+      namespace = "poketwo";
+    }];
+    roleRef = {
+      kind = "ClusterRole";
+      name = "admin";
+      apiGroup = "rbac.authorization.k8s.io";
+    };
+  };
+in
 {
   namespaces.poketwo.resources = {
     v1.ServiceAccount.witherr = { };
-
-    "rbac.authorization.k8s.io/v1".RoleBinding.witherr = {
-      subjects = [{
-        kind = "ServiceAccount";
-        name = "witherr";
-      }];
-      roleRef = {
-        kind = "ClusterRole";
-        name = "admin";
-        apiGroup = "rbac.authorization.k8s.io";
-      };
-    };
-
-    "rbac.authorization.k8s.io/v1".ClusterRoleBinding.witherr = {
-      subjects = [{
-        kind = "ServiceAccount";
-        name = "witherr";
-        namespace = "poketwo";
-      }];
-      roleRef = {
-        kind = "ClusterRole";
-        name = "view";
-        apiGroup = "rbac.authorization.k8s.io";
-      };
-    };
+    "rbac.authorization.k8s.io/v1".RoleBinding.witherr = adminRoleBinding;
+    "rbac.authorization.k8s.io/v1".ClusterRoleBinding.witherr = adminRoleBinding;
   };
+
+  namespaces.guiduck.resources."rbac.authorization.k8s.io/v1".RoleBinding.witherr = adminRoleBinding;
+  namespaces.poketwo-staging.resources."rbac.authorization.k8s.io/v1".RoleBinding.witherr = adminRoleBinding;
+  namespaces.poketwo-staging-private.resources."rbac.authorization.k8s.io/v1".RoleBinding.witherr = adminRoleBinding;
 }
