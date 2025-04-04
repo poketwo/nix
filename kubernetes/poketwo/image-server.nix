5c01999076fb48ca19d4b58c9fe46ed2cfd2fb4d
Witherr <sdssouvic99@gmail.com>
2025-04-05 01:22:08 +0530

    Increase number of image-server replicas

--- kubernetes/poketwo/image-server.nix
+++ kubernetes/poketwo/image-server.nix
@@ -4,7 +4,7 @@
   namespaces.poketwo = {
     resources = {
       "apps/v1".Deployment.image-server.spec = {
-        replicas = 15;
+        replicas = 20;
         selector.matchLabels.app = "image-server";
         template = {
           metadata.labels.app = "image-server";
