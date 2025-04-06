1a85ccd26c5ee8b113a0299a322fb30f39eac372
Witherr <sdssouvic99@gmail.com>
2025-04-06 23:30:25 +0530

    Increase poketwo cpu limits

--- kubernetes/poketwo/poketwo.nix
+++ kubernetes/poketwo/poketwo.nix
@@ -27,7 +27,7 @@
               imagePullPolicy = "Always";
               resources = {
                 limits = { memory = "4Gi"; };
-                requests = { memory = "2Gi"; cpu = "100m"; };
+                requests = { memory = "2Gi"; cpu = "300m"; };
               };
               envFrom = [{ secretRef.name = "poketwo"; }];
               env = [
